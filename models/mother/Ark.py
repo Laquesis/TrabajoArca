1d179f51705c86cab721b8083be803ba6bb20767
carmen ramirez garcia <carmenrag12@gmail.com>
2024-11-06 19:42:31 +0100

    check point

--- models/mother/Ark.py
+++ models/mother/Ark.py
@@ -1,5 +1,5 @@
 class Ark:
-    def __init__(self, animals=None, foods=None,water=None max_capacity=None):
+    def __init__(self, animals=None, foods=None,water=None, max_capacity=None, left=False):
         # Inicializar listas si no se pasan como argumentos
         if animals is None:
             animals = []
@@ -8,34 +8,54 @@ class Ark:
         if water is None:
             water = 0
         if max_capacity is None:
-            max_capacity = {"animal": 100, "food": 1000, "water": 10000}
-        
+            max_capacity = {"animal": 100, "food": 1000, "water": 10000}         
+
+               
         # Asignación a los atributos de la clase
         self.animals = animals
         self.food = foods
         self.water = water
         self.max_capacity = max_capacity
+        self.left=left
+    
+    def left_ark(self, left):
+        self.left=True
 
     def add_animal(self, animal):
         """Agregar un animal si hay capacidad disponible."""
-        if len(self.animal) < self.max_capacity["animal"] :
+        if len(self.animal) < self.max_capacity["animal"]  and self.left=False:
             self.animals.append(animal)           
         else:
-            print("No hay más capacidad para añadir animales.")
+            print("No se pueden añadir más animales.")
 
     def add_food(self, food):
         """Agregar comida si hay capacidad disponible."""
-        if len(self.food) < self.max_capacity["food"]:
+        if len(self.food) < self.max_capacity["food"] :
             self.food.append(food)       
         else:
-            print("No hay más capacidad para añadir comida.")
+            print("No se puede añadir más comida.")
 
     def add_water(self, water):
         """Agregar agua si hay capacidad disponible."""
         if self.water < self.max_capacity["water"]:
             self.water +=water          
         else:
-            print("No hay más capacidad para añadir agua.")
+            print("No se puede añadir más agua.")
+
+     def add_multi_animal(self, list_animal):
+        """Agregar un animal si hay capacidad disponible."""
+        if len(self.list_animal) < self.max_capacity["animal"]  and self.left=False:
+            self.animals.extend(self.list_animal)           
+        else:
+            print("No se pueden añadir más animales.")
+
+    def add_multi_food(self, list_food):
+        """Agregar comida si hay capacidad disponible."""
+        if len(self.list_food) < self.max_capacity["food"] :
+            self.food.extend(self.list_food)       
+        else:
+            print("No se puede añadir más comida.")
+  
 
     def get_status(self):
         """Mostrar el estado actual de la capacidad."""
@@ -43,5 +63,8 @@ class Ark:
             "animals": len(self.animals),
             "food": len(self.food),
             "water": self.water,
-            "max_capacity": self.max_capacity
+            "max_capacity": self.max_capacity,
+            "left": self.left
         }
+    def search_food(self,food):
+
--- models/mother/Food.py
+++ models/mother/Food.py
@@ -1,12 +1,46 @@
+
+import Animal from Animal.py
+import Ark from Ark.py
 class Food:
-    
-    def _init_(self, tipo=None, cantidad=None):
+    #tipos de alimento
+    #vegetal=0
+    #animal = 1
+    def _init_(self, name, tipo=None, cantidad=None):
+        self.name=name
         self.tipo=tipo
         try:
             isinstance(cantidad , bool ) 
         except ValueError:
              raise "cantidad tiene que ser positivo"
-        self.cantidad=0
+        self.calorias=1
     
-    def crear_alimento(self,tipo, cantidad):
+    def crear_alimento(self,name, tipo, calorias):
+        self.name=name
+        self.tipo=tipo
+        self.calorias+=calorias
+
+    def is_suitable(self, animal_type):
+        #herbivoro =0
+        #carnivoro=1
+        #omnivoro=2
+        suitable=False       
+        match self.animal_type:
+            case 0 :
+                if self.tipo==0:
+                    suitable=True
+                else:
+                    suitable=False              
+            case 1:
+                if self.tipo==0:
+                    suitable=False
+                else:
+                    suitable=True
+            case 2:
+                suitable=True
+            else:
+                suitable=False
+        
+        return suitable
+                
+
 
